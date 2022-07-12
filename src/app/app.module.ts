4cfa2044e3a1858818ad14e44212589e1fa4a9e4
artuzo <geferson.artuzo@viasoft.com.br>
2022-07-12 18:24:13 -0300

    more studies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,38 @@
-import { registerLocaleData } from '@angular/common';
-import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+import {registerLocaleData} from '@angular/common';
+import {DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { ExtractComponent } from './extract/extract.component';
-import { NewTransferComponent } from './new.transfer/new.transfer.component';
+import {AppComponent} from './app.component';
+import {ExtractComponent} from './extract/extract.component';
+import {NewTransferComponent} from './new.transfer/new.transfer.component';
 import localePT from '@angular/common/locales/pt';
-import { HttpClientModule } from '@angular/common/http';
+import {HttpClientModule} from '@angular/common/http';
 import {AppRoutingModule} from './app.routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {InitialComponent} from './initial/initial.component';
+import {RemoveComponent} from './remove/remove.component';
+
 
 registerLocaleData(localePT, 'pt');
 
 @NgModule({
   declarations: [
+    InitialComponent,
     AppComponent,
     NewTransferComponent,
-    ExtractComponent
+    ExtractComponent,
+    RemoveComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+
   ],
   providers: [
     {provide: LOCALE_ID, useValue: 'pt-BR'},
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -2,18 +2,22 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {ExtractComponent} from './extract/extract.component';
 import {NewTransferComponent} from './new.transfer/new.transfer.component';
+import {RemoveComponent} from './remove/remove.component';
+import {InitialComponent} from './initial/initial.component';
 
-  export const routes: Routes = [
-    {path: '', redirectTo: 'extract', pathMatch: 'full'},
-    {path: 'extract', component: ExtractComponent},
-    {path: 'new-transfer', component: NewTransferComponent},
+export const routes: Routes = [
+  {path: '', redirectTo: 'initial', pathMatch: 'full'},
+  {path: 'initial', component: InitialComponent},
+  {path: 'extract', component: ExtractComponent},
+  {path: 'new-transfer', component: NewTransferComponent},
+  {path: 'remove', component: RemoveComponent},
 
-  ];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
-  })
-export class AppRoutingModule{
+})
+export class AppRoutingModule {
 
 }
